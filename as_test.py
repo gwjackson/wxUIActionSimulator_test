b51a26fe36ba3b36ba44a7efb5f54d735da04d50
gwjac <gwjackson53@gmail.com>
2026-08-08 14:16:53 -0600

    some basic widgets, more to add

--- as_test.py
+++ as_test.py
@@ -2,8 +2,13 @@ import wx
 from src.as_test.gui.main_frame import MainFrame
 
 def main():
+    """
+    This is the main function and will call the GUI, Utilities, Core functions etc.
+    :return: None
+    """
     app = wx.App(False)
     frame = MainFrame()
+    frame.Fit()
     frame.Show()
     app.MainLoop()
 
