a86d4e06029904564c137de54c1b6355304a521e
gwjac <gwjackson53@gmail.com>
2026-08-28 20:17:51 -0600

    add macrocontroller and decorator and needs testing, need to fix on_play()

--- src/as_test/core/macro_widget.py
+++ src/as_test/core/macro_widget.py
@@ -1,5 +1,6 @@
 import wx
 import time
+import functools
 
 
 
@@ -12,7 +13,8 @@ class UI_Simulator(wx.UIActionSimulator):
         super().__init__()
         pass
 
-
+# this needs to be moved down to the new Macro class
+# MacroManager to be in that class
 def on_record(record, playback, evt=None):
     """
     use a lamda function in the .Bind
@@ -24,22 +26,18 @@ def on_record(record, playback, evt=None):
     build the macro list of moves / clicks / chars / etc.
     :return:
     """
+    macros = MacroManager()
 
     if record.GetLabel() == "🔴 Record Macro":
         record.SetLabel("⏹ Stop Recording")
         playback.Disable()
         start_time = time.time()
-        recording = True
-        _start_record_macro(recording)
+        macros.start_recording()
     else:
         record.SetLabel("🔴 Record Macro")
         playback.Enable()
-        print("Stopped recording")
+        macros.stop_recording("Test Macro")
         return
-
-def _start_record_macro(recording):
-        print("recording")
-        pass
 
 # ------------------------------------------------
 # Menu selections
@@ -98,3 +96,116 @@ def on_dlg_button(self, evt):
         print("Event ID:", evt.GetId())
         print("Timestamp:", evt.GetTimestamp())
 """
+
+# ------------------------------------------------------------------
+# Command Classes
+# ------------------------------------------------------------------
+class MacroCommand(wx.Command):
+    def __init__(self, widget, new_value, old_value, label="Macro Command"):
+        super().__init__(True, label)
+        self.widget = widget
+        self.new_value = new_value
+        self.old_value = old_value
+
+    def Do(self ):
+        self.widget.SetValue(self.new_value)
+        return True
+
+    def Undo(self ):
+        self.widget.SetValue(self.old_value)
+        return True
+
+
+class CompositeMacroCommand(wx.Command):
+    def __init__(self, actions, label="Composite Macro"):
+        super().__init__(True, label)
+        self.actions = actions                      #list of (widget, new, old)
+
+    def Do(self ):
+        for widget, new_value, old_value in self.actions:
+            widget.SetValue(new_value)
+        return True
+
+    def Undo(self ):
+        for widget, new_value, old_value in self.actions:
+            widget.SetValue(old_value)
+        return True
+
+
+class MacroManager:
+    def __init__(self):
+        self.processor = wx.CommandProcessor()
+        self.recording = False                                      #should this be read from the app Macro button?
+        self.record_buffer = []
+        self.user_macros = {}
+
+    # --------------------------------
+    # Recording control
+    # --------------------------------
+    def start_recording(self):                                            # toggle record macro / stop recording: to have to refractor this from above
+        self.recording = True
+        self.record_buffer.clear()
+        print("Macro started recording")
+
+    def stop_recording(self, macro_name):
+        self.recording = False
+        self.user_macros[macro_name] = list(self.record_buffer)
+        print("Macro stopper recording")
+
+
+    # -----------------------------------------
+    #  Decorator
+    # -----------------------------------------
+    """
+    Every widget / hotkey etc. that you want to include in a macro has to 
+    1. run it's business logic i.e. what you want the widget to do launch record etc. 
+    2. has to capture is initial state
+    3  had to capture is final state (this is for the Do / Undo function of command)
+    4. append its self on to the record_buffer
+    Now that is a lot of boiler plate for every widget -- > so a decorature just makes sense
+    And you need to only decorate user generated events that you want to record, not all your widgets
+    For instance you do not what to record the 'Record', 'Stop Recording', 'Playback' buttons you 
+    would create a crazy loop and cause a tear in the space-time continum 
+    """
+    def recoradable(self, widget_getter):
+        def decorator(func):
+            @functools.wraps(func)
+            def wrapper(*args, **kwargs):
+                # 1 run the business logic
+                result = func(*args, **kwargs)
+
+                if self.recording:
+                    # @self.macros.recordable(lambda self, widget, event: widget) is for the widget handler
+                    widgets = widget_getter(*args, **kwargs)
+
+                    if not isinstance(widgets, (list, tuple)):
+                        widgets = [widgets]
+
+                    for widget in widgets:
+                        new_value = widget.GetValue()
+                        self.record_buffer.append((widget, new_value, new_value))
+                return result
+            return wrapper
+        return decorator
+
+    # ----------------------------------------
+    # Replay macro
+    # ________________________________________
+    def apply_macro(selfself, macro_name):
+        actions = self.user_macros.get(macro_name)
+        if not actions:
+            return
+
+        cmd = CompositeMacroCommand(actions, label=f'Macro: {macro_name}')
+        self.processor.Submit(cmd)
+
+    # ---------------------------------------------
+    # Undo / Redo  (not sure will be using but is part of Command / CommandProcessor
+    # ----------------------------------------------
+    def undo(self):
+        self.processor.Undo()
+
+    def redo(self):
+        self.processor.Redo()
+
+
